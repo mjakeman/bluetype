377fa8c84573c80da4ef4ac547677ba434d9afd2
Matthew Jakeman <mjakeman26@outlook.co.nz>
2021-11-11 03:17:30 +1300

    Use gtk-blueprint-tool instead of GtkBuilder XML

--- src/bl-window.c
+++ src/bl-window.c
@@ -25,7 +25,7 @@ struct _BlWindow
 
   /* Template widgets */
   GtkHeaderBar        *header_bar;
-  GtkLabel            *label;
+  GtkTextView            *textview;
 };
 
 G_DEFINE_TYPE (BlWindow, bl_window, GTK_TYPE_APPLICATION_WINDOW)
@@ -36,8 +36,8 @@ bl_window_class_init (BlWindowClass *klass)
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
   gtk_widget_class_set_template_from_resource (widget_class, "/com/mattjakeman/Bluetype/bl-window.ui");
-  gtk_widget_class_bind_template_child (widget_class, BlWindow, header_bar);
-  gtk_widget_class_bind_template_child (widget_class, BlWindow, label);
+  // gtk_widget_class_bind_template_child (widget_class, BlWindow, header_bar);
+  // gtk_widget_class_bind_template_child (widget_class, BlWindow, textview);
 }
 
 static void
