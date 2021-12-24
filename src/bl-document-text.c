3461fb2448b252614d3ace299922e1139b8c5ff3
Matthew Jakeman <mjakeman26@outlook.co.nz>
2021-12-24 19:13:39 +1300

    project: Relicence to MPL-2.0

--- src/bl-document-text.c
+++ src/bl-document-text.c
@@ -2,20 +2,11 @@
  *
  * Copyright 2021 Matthew Jakeman <mjakeman26@outlook.co.nz>
  *
- * This file is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of the
- * License, or (at your option) any later version.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *
- * This file is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: LGPL-3.0-or-later
+ * SPDX-License-Identifier: MPL-2.0
  */
 
 #define G_LOG_DOMAIN "bluetype-document-text"
